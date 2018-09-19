2a6bc08b4db224814099ed61da0b6f17c01178a7
Fernando <fvk113@gmail.com>
2018-09-19 17:20:52 -0300

    origin

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"time"
+)
 
+func main() {
+	db, err := gorm.Open("postgres", "user=convenios password=convenios dbname=conveniosweb sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = db.DB().Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Conexión exitosa")
+	db.SingularTable(true)
+	var acc Account
+	db.AutoMigrate(Account{})
+	db.Debug().First(&acc)
+	fmt.Println(acc)
+	db.Close()
+}
+
+type Account struct {
+	ID         string
+	EMAIL      string
+	FIRST_NAME string
+	LAST_NAME  string
+	LOGIN      string
+	PASSWORD   string
+	ROLE_NAME  string
+	IS_ENABLED bool
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  *time.Time `sql:"index"`
 }
--- struct/conce.go
+++ struct/conce.go
@@ -1,5 +1,1 @@
 package dominio
-
-func main() {
-
-}
